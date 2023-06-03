2872424efc141487bf16ec236c5e4ec6aafe3e4c
SeungTaeGit <129585999+SeungTaeGit@users.noreply.github.com>
2023-06-03 23:19:58 +0900

    Add files via upload

---
+++ POKEYforgot/src/main/java/capstone/pokey/POKEYforgot/PokeYforgotApplication.java
@@ -0,0 +1,13 @@
+package capstone.pokey.POKEYforgot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeYforgotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeYforgotApplication.class, args);
+	}
+
+}
---
+++ POKEYforgot/src/test/java/capstone/pokey/POKEYforgot/PokeYforgotApplicationTests.java
@@ -0,0 +1,13 @@
+package capstone.pokey.POKEYforgot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeYforgotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
